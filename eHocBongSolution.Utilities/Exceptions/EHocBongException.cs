5d52551217068573854feddd7fc93313b541e36d
ngochuy20000614 <67599441+ngochuy20000614@users.noreply.github.com>
2021-03-18 23:36:58 +0700

    Design CRUD for Application Product

---
+++ eHocBongSolution.Utilities/Exceptions/EHocBongException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eHocBongSolution.Utilities.Exceptions
+{
+    public class EHocBongException : Exception
+    {
+        public EHocBongException()
+        {
+        }
+
+        public EHocBongException(string message)
+            : base(message)
+        {
+        }
+
+        public EHocBongException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
