75a73ad990ac86819d598086d810b1ad94d1838d
ngochuy20000614 <67599441+ngochuy20000614@users.noreply.github.com>
2021-04-05 17:45:53 +0700

    Add JWT token to Swagger

--- eShopSolution.BackendApi/Controllers/ProductsController.cs
+++ eShopSolution.BackendApi/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using eShopSolution.Application.Catalog.Products.Public;
 using eShopSolution.ViewModels.Catalog.ProductImages;
 using eShopSolution.ViewModels.Catalog.Products;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace eShopSolution.BackendApi.Controllers
 
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
